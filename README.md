e65c69febec78e1c0c59fe893466b5e5b11a8c91
littlefieldnick <nickolas.littlefield@maine.edu>
2021-12-12 14:04:21 -0500

    Updated README and added missing main.py file

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# cos527-pubmed-classification
-Final Project for COS 527: Computational Text Analytics (Fall 2021)
+# Pubmed Abstract Classification
+## Nick Littlefield
+
+### Overview
+New biomedical research articles are being published every day. By using text mining on large scale scientific literature, we can discover new knowledge to better understand human diseases and help to improve the quality of disease diagnosis, prevention, and treatment. In this project, the goal is to build a text classification system that downloads abstracts from Pubmed to extract and classify four different diseases: lyme disease, acute rheumatic arthritis, and cardiovascular abnormalities. 
+
+### Materials and Methods
+There are three tiers to to this classification system:
+1. Data Access
+2. Text Preprocessing
+3. Machine Learning and Classification
+
+#### Tier 1: Data Access
+Roughly 77,000 abstracts were downloaded from Pubmed using the Biopython library. These abstracts along with their corresponding class were saved to a CSV file for later preprocessing. Any abstracts that downloaded that had no text were discarded. 
+
+#### Tier 2: Text Preprocessing
+Text processing was done on the saved abstracts using the `nltk` library. The following preprocessing steps were done:
+1. Removal of punctuation and numbers
+2. Text normalization: all text converted to lowercase
+3. Text was split into lists of tokens
+4. Lemmatization and stemming applied to tokens
+5. Preprocessed text rejoined into a string for use in the machine learning tier
+
+After preprocessing the abstracts, they were split into a training and test set using a 80/20 split. The train and test set were saved in individual CSV files for use in the machine learning tier.
+
+#### Tier 3: Machine Learning
+Three machine learning algorithms were used for classification via the `sklearn` library. These algorithms were:
+1. Naive Bayes
+2. Logistic Regression
+3. Support Vector Machines
+
+The abstracts used contains a large imbalance between the different classes. To handle this imbalance the 'imblearn' library was used to oversample the minority classes. 
+
+To perform the training process a pipeline was built to perform a series of steps. These steps were:
+1. Convert training data to a bag of words representation
+2. Apply TF-IDF to the bag of words representation
+3. Oversample the minority classes
+4. Train the machine learning algorithm.
+
+By using this pipeline when it comes time to evaluate the model, the pipeline will also convert the test abstracts to a Bag of Words representation and apply TF-IDF. The oversampling step is ignored when predictions are made. 
+
+### Results
+The results for each of the machine learning algorithms are below. For each model, the confusion matrix, F1 score, precision, recall, and support are provided. 
+
+#### Naive Bayes
+
+#### Logistic Regression
+
+#### Support Vector Machine
+
+### Conclusions
+
+### Discussion
+
+### Future Outlooks
---
+++ main.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 18:00:28 2021
+
+@author: nicklittlefield
+
+=========================================================
+
+main.py: Main script that runs abstract downloads and runs machine learning classification
+         algorithms. 
+"""
+
+import os
+import pickle
+
+import numpy as np
+import pandas as pd
+
+from download_abstract import PubmedEntrezAPI
+from preprocessing import gen_train_test_split
+from ml import save_models, load_models, train_nb_pipeline, train_svm_pipeline, \
+        train_logistic_regression_pipeline, \
+        eval_model
+
+train_models = True
+data_pth = "/Users/nicklittlefield/Desktop/cos527-pubmed-classification/data/"
+model_pth = "/Users/nicklittlefield/Desktop/cos527-pubmed-classification/saved_models/"
+
+def data_access_tier(terms, entrez_params, start_date, end_date=None, batch_size=100):
+    # Create Pubmed API object to download abstracts
+    api = PubmedEntrezAPI(terms, entrez_params, date_start=start_date, date_end=end_date, batch_size=batch_size)
+
+    # Download abstracts
+    api.load_data()
+    
+    # Get the abstracts
+    abstracts = api.get_data()
+    
+    # Save them to file
+    abstracts.to_csv(data_pth + "abstract.csv")
+    
+
+def preprocess_tier(abstract_pth):
+    abstracts = pd.read_csv(abstract_pth)
+    abstracts.dropna(inplace=True) # NaNs are in the dataset... Not sure why, so lets drop them.
+    train, test = gen_train_test_split(abstracts)
+    
+    # Save the outputs
+    train.to_csv("data/train.csv", index=False)
+    test.to_csv("data/test.csv", index=False)
+
+
+def ml_train_tier(train_data, save_pipelines=True):
+    nb_pipeline = train_nb_pipeline(train)
+    log_pipeline = train_logistic_regression_pipeline(train)
+    svm_pipeline = train_svm_pipeline(train)
+    
+    if save_pipelines:
+        save_models(model_pth, nb_pipeline, log_pipeline, svm_pipeline)
+
+    return nb_pipeline, log_pipeline, svm_pipeline
+
+def ml_eval_tier(test_data, nb, log, svm, labels):
+    eval_model(nb, test_data, labels, "Naive Bayes")
+    eval_model(log, test_data, labels, "Logistic Regression")
+    eval_model(svm, test_data, labels, "Support Vector Machine")
+
+
+if not os.path.exists(data_pth):
+    os.mkdir(data_pth)
+    
+if not os.path.exists(model_pth):
+    os.mkdir(model_pth)
+    
+    
+#############################
+# Tier 1: Data Access       #
+#############################
+    
+# Parameters for entrez search
+entrez_params = {
+    "retmode": "xml",
+    "rettype": "abstract",
+    "usehistory": "y",
+}
+
+# MeSH Terms for Pubmed
+terms = ["disease, lyme", "abnormalities, cardiovascular", "knee osteoarthritis",
+         "acute rheumatic arthritis"]
+
+
+# Check if abstracts are downloaded, if not download them
+if not os.path.exists(data_pth + "abstract.csv"):
+    abstracts = data_access_tier(terms, entrez_params, "01/01/2010", batch_size=5000)
+    
+
+#############################
+# Tier 2: Preprocessing     #
+#############################
+
+# Check if train and test splits if they don't exist generate the splits and save them
+if not os.path.exists(data_pth + "train.csv") or not os.path.exists(data_pth + "test.csv"):
+    # Load abstracts
+    abstracts = pd.read_csv("data/abstracts.csv")
+    abstracts.dropna(inplace=True) # NaNs are in the dataset... Not sure why, so lets drop them.
+
+    # Get train/test splits
+    train, test = gen_train_test_split(abstracts)
+else:
+    train = pd.read_csv(data_pth + "train.csv")
+    test = pd.read_csv(data_pth + "test.csv")
+
+
+#############################
+# Tier 3: Machine Learning  #
+#############################
+
+train.dropna(inplace=True)
+test.dropna(inplace=True)
+
+if train_models:
+    nb_pipeline, log_pipeline, svm_pipeline = ml_train_tier(train)
+else:
+    nb_pipeline, log_pipeline, svm_pipeline = load_models(train)
+
+ml_eval_tier(test, nb_pipeline, log_pipeline, svm_pipeline, terms)
+
